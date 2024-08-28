1dceca5bf893806d6e7a9fe7eda17ae96826af0a
Rociolopez999 <rm867288@gmail.com>
2024-08-28 14:13:03 -0600

    Agregar archivos de proyecto.

---
+++ programa en clase wilmer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] arg)
+    {
+        bool continuar = true;
+        while (continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("=== Ventas Varias ===");
+            Console.WriteLine("1. Transistores");
+            Console.WriteLine("2. Capacitores");
+            Console.WriteLine("3. Leds");
+            Console.WriteLine("4. Salir");
+            Console.WriteLine("=====================");
+            Console.Write("Seleccione una opcion: ");
+
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("Has Seleccionado la Opcion 1.");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Has Seleccionado la Opcion 2.");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Has Seleccionado la Opcion 2.");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Has Seleccionado la Opcion 2.");
+                    continuar = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opcion ni Valida. Intente de Nuevo.");
+                    break;
+
+
+            }
+
+            if (continuar)
+            {
+                Console.WriteLine("Precione Cualquier tecla Para continuar ...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
+
